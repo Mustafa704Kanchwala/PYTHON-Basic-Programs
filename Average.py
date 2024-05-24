25d010f3bdc599cd987e212f4cc4b567d49f73fb
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-24 11:51:10 +0530

    [ADDED]:Mustafa - This PYTHIN program gives the average

---
+++ Average.py
@@ -0,0 +1,8 @@
+num1 = input("Enter the first Number:-")
+num2 = input("Enter the Second Number:-")
+
+num1=int(num1)#<<== Convertin string into INT here #
+num2=int(num2)#<<== Convertin string into INT here #
+avr = (num1 + num2) /2
+
+print("Average =",avr)
