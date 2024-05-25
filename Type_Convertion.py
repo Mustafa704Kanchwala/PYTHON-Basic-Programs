5da2e740fad777f2e2869f92ae9473f5c16e774f
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-25 16:10:10 +0530

    [ADDED]:Mustafa - This PYTHIN program demonstrate how to change type of a variable

---
+++ Type_Convertion.py
@@ -0,0 +1,8 @@
+a = 0 # a is a integer(int) here
+print("Before convertion a=",type(a))
+
+#you can Covert the type of variable the way you want 
+#By using "int()" "floa()" "str()" etc...
+
+a = str(a) # a is string variable now
+print("\nAfter the Convertion a =",type(a))
