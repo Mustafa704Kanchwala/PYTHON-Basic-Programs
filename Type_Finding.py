3f3d1f081ed47800707982d185c10e64c356b764
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-25 15:45:52 +0530

    [ADDED]:Mustafa - This PYTHON program shows the use of type()

---
+++ Type_Finding.py
@@ -0,0 +1,9 @@
+# This program is use for findin the DATA_TYPE of  varialbe #
+name = "Mustafa"
+age =21
+height = 5.11
+
+# Printing the type of variable using type() #
+print("Data type of name = ",type(name)) # Output = <class 'str'>
+print("Data type of age = ",type(age)) # Output = <class 'int'>
+print("Data type of height = ",type(height)) #  Output = <class 'float'>
