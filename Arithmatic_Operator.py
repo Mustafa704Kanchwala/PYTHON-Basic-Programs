b32d07fe25ffd871a18635594c93e20727bbc84d
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-26 14:41:58 +0530

    [ADDED]:Mustafa -  This is a program to demonstrate arithmatic operator's in PYTHON

---
+++ Arithmatic_Operator.py
@@ -0,0 +1,8 @@
+#Python also supports arithmatic operatoins #
+num1 = int(input("Enter the first value:-"))
+num2 =int(input("Enter the Second value:-"))
+
+print("\nAddition:- ",num1+num2)
+print("Substraction:-",num1-num2)
+print("Division:-",num1/num2)
+print("Reminder:-",num1%num2)
