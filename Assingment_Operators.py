82f7a9ec1d833b5619f320c2feed283ead8ee6ec
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-26 15:47:05 +0530

    [ADDED]:Mustafa - This PYTHON program show how Assingment operator Works

---
+++ Assingment_Operators.py
@@ -0,0 +1,20 @@
+#This is how ASSINGMENT_OPERATORS wor in PYTHON#
+value =10
+
+value += 1 #Answer = 31
+print("Value:- ", value)
+
+value -= 2 #Answer = 29
+print("Value:- ", value)
+
+value *= 10 #Answer = 290
+print("Value:- ", value)
+
+value /=10 #Answer = 29.0
+print("Value:- ", value)
+
+value **= 5 #Answer = 59049.0
+print("Value:- ",value)
+
+value %= 2 #Answer = 1.0
+print("Value:- ",value)
