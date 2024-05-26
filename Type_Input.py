4b778b420f26d07fe5acb380f12cf8d54f31c008
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-26 21:28:06 +0530

    [ADDED]:Mustafa - This program shows the how to define Data_Type of variable while taking input

---
+++ Type_Input.py
@@ -0,0 +1,6 @@
+# In this program we are going to see How define type of variable while taking input #
+num1 = int(input("Enter an Integer Value:-"))
+num2 = float(input("Enter an Float value:-"))
+
+print("Num1 is",type(num1),num1)
+print("Num2 is",type(num2),num2)
