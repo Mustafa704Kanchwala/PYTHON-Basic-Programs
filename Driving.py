94d41d0aa4ff3d52a87c38ffd36cfe9291c2643e
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-27 13:38:37 +0530

    [ADDED]:Mustafa - This is a basic age Checking program in PYTHON

---
+++ Driving.py
@@ -0,0 +1,6 @@
+name = input("Enter the name of Candidate:- ")
+age = int(input("\nEnter the age:- "))
+if(age >= 18):
+    print(name,"is Eligible for Driving")
+else:
+    print(name,"is Not-Eligible for Driving") 
