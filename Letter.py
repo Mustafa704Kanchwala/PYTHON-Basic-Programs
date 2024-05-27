381f084e7b7236247c3d899dc8253750350604db
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-05-27 20:10:55 +0530

    [ADDED]:Mustafa - This program demonstrate how to format text in variable  in python

---
+++ Letter.py
@@ -0,0 +1,5 @@
+letter = "Hey! tere, this is a python program for demonstrate the text formating in python...."
+print(letter)
+
+formated_letter = "\nHey there !!!\n\tthis is a program for demonstrate\n\tthe text formating in python....\n\t\tTHANKS!!!!"
+print(formated_letter)
