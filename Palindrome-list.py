bb6e2c9a158ffb641c4555a2148e02add9769650
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-08-19 15:23:53 +0530

    [ADDED]:Mustafa - This is a pythin program for checking that given list is palindrome or not

--- Palindrome-list.py
+++ Palindrome-list.py
@@ -1,9 +1,10 @@
 list1 = [1,2,4,2,1]
 copy_list1 = list1.copy()
+copy_list1.reverse()
 
 if copy_list1 == list1:
   print("List is Palindrome:")
 else:
-  pritn("Not a palindrome list")
+  print("Not a palindrome list")
 
 
